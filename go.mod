94a09fb632a8e7d68e29aaf86789e2efbe398495
Lee ByeongJun <lbj199874@gmail.com>
2024-04-30 17:32:13 +0900

    more graphs

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/andybalholm/stroke v0.0.0-20221221101821-bd29b49d73f0 // indirect
 	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-fonts/liberation v0.3.2 // indirect
 	github.com/go-latex/latex v0.0.0-20231108140139-5c1ce85aa4ea // indirect
 	github.com/go-pdf/fpdf v0.9.0 // indirect
@@ -18,6 +19,7 @@ require (
 	github.com/goccmack/gocc v0.0.0-20230228185258-2292f9e40198 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
 	golang.org/x/exp/shiny v0.0.0-20230801115018-d63ba01acd4b // indirect
 	golang.org/x/image v0.15.0 // indirect
@@ -27,5 +29,6 @@ require (
 	golang.org/x/tools v0.20.0 // indirect
 	gonum.org/v1/gonum v0.15.0 // indirect
 	gonum.org/v1/plot v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/pdf v0.1.1 // indirect
 )
--- plot/main.go
+++ plot/main.go
@@ -1,44 +1,105 @@
 package main
 
 import (
+	"image/color"
+	"math/rand"
+	"sync"
+	"time"
+
 	"github.com/notJoon/pcg"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 )
 
-func main() {
-	pcg := pcg.NewPCG32().Seed(12345, 67890)
-	numBins := 25
-	numSamples := 1000000
-	bins := make([]int, numBins)
+const (
+	numBins    = 100
+	numSamples = 25000
+)
 
+func generateRandHistogram(bins []int, seed int64) {
+	r := rand.New(rand.NewSource(seed))
+	for i := 0; i < numSamples; i++ {
+		binIndex := r.Intn(numBins)
+		bins[binIndex]++
+	}
+}
+
+func generatePCG32Histogram(bins []int, seed int64) {
+	pcg := pcg.NewPCG32().Seed(uint64(seed), uint64(seed))
 	for i := 0; i < numSamples; i++ {
 		r := pcg.Uint32()
 		binIndex := int(uint64(r) * uint64(numBins) >> 32)
 		bins[binIndex]++
 	}
+}
 
-	pl := plot.New()
+func main() {
+	seed := time.Now().Unix()
 
-	pl.Title.Text = "PCG32 Uniform Distribution"
-	pl.X.Label.Text = "Bin"
-	pl.Y.Label.Text = "Count"
+	binsRand := make([]int, numBins)
+	binsPCG := make([]int, numBins)
 
-	w := vg.Points(20)
-	bars := make(plotter.Values, numBins)
-	for i := range numBins {
-		bars[i] = float64(bins[i])
-	}
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	bar, err := plotter.NewBarChart(bars, w)
+	go func() {
+		defer wg.Done()
+		generateRandHistogram(binsRand, seed)
+	}()
+	go func() {
+		defer wg.Done()
+		generatePCG32Histogram(binsPCG, seed)
+	}()
+
+	wg.Wait()
+
+	// Creating plots
+	pRand := plot.New()
+	pRand.Title.Text = "math/rand Distribution"
+	pRand.X.Label.Text = "Bin"
+	pRand.Y.Label.Text = "Count"
+
+	pPCG32 := plot.New()
+	pPCG32.Title.Text = "PCG Distribution"
+	pPCG32.X.Label.Text = "Bin"
+	pPCG32.Y.Label.Text = "Count"
+
+	pPCG64 := plot.New()
+	pPCG64.Title.Text = "PCG64 Distribution"
+	pPCG64.X.Label.Text = "Bin"
+	pPCG64.Y.Label.Text = "Count"
+
+	// Rand histogram
+	randHistogram, err := plotter.NewBarChart(plotter.Values(makePlotterValues(binsRand)), vg.Points(5))
 	if err != nil {
 		panic(err)
 	}
+	randHistogram.Color = color.RGBA{R: 255, A: 255} // Red
 
-	pl.Add(bar)
-
-	if err := pl.Save(4*vg.Inch, 4*vg.Inch, "uniform_distribution_bin25.png"); err != nil {
+	// PCG32 histogram
+	pcg32Histogram, err := plotter.NewBarChart(plotter.Values(makePlotterValues(binsPCG)), vg.Points(5))
+	if err != nil {
 		panic(err)
 	}
+	pcg32Histogram.Color = color.RGBA{B: 255, A: 255} // Blue
+
+	pRand.Add(randHistogram)
+	pPCG32.Add(pcg32Histogram)
+
+	if err := pRand.Save(8*vg.Inch, 4*vg.Inch, "rand_histogram.png"); err != nil {
+		panic(err)
+	}
+
+	if err := pPCG32.Save(8*vg.Inch, 4*vg.Inch, "pcg32_histogram.png"); err != nil {
+		panic(err)
+	}
+}
+
+func makePlotterValues(bins []int) []float64 {
+	values := make([]float64, len(bins))
+	for i, count := range bins {
+		values[i] = float64(count)
+	}
+	return values
 }
